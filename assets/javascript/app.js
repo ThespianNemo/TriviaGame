c7d09c28bea2e8928ac1a4f7ac87d171b47561aa
Preston Cropp <Preston@Christophers-MacBook-Air.local>
2018-02-13 20:47:51 -0600

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ var correctAnswer = 0;
 var incorrectAnswer = 0;
 var unAnswered = 0;
 var correct;
+var qcounter = 0;
 
 //Initialize screen answer assignments
 var one = 1;
@@ -11,16 +12,16 @@ var two = 2;
 var three = 3;
 var four = 4;
 
-//  Set The number counter to 21.
-var number = 21;
+//  Set the timer to 21 seconds
+var timer = 21;
 
 //  Variable that will hold the interval ID when executing
 //  the "run" function
 var intervalId;
 
 //Question and answer array
-var questionList = {
-    film1: {
+var questionList = [
+     {
         question: "Which war movie won the Academy Award for Best Picture in 2009?",
         answers: [
             "Inglourious Basterds",
@@ -28,9 +29,9 @@ var questionList = {
             "The Hurt Locker",
             "The Messenger",
             "The Hurt Locker"]
-         },
+        },
      
-      film2: {
+       {
          question: "In what year was the original 'Jurassic Park' film released?",
          answers: [
              "2006",
@@ -40,7 +41,7 @@ var questionList = {
              "1993"]
          },
      
-      film3:{
+      {
          question: "What are the four houses at Hogwarts School of Witchcraft and Wizardry?",
          answers: [
              "Gryffindor, Ravenclaw, Huffinpuff, Slytherin",
@@ -50,7 +51,7 @@ var questionList = {
              "Gryffindor, Ravenclaw, Hufflepuff, Slytherin"]   
          },
      
-      film4: {
+       {
          question: "In the movie Mean Girls, where is Caty originally from?",
          answers: [
              "Africa",
@@ -60,7 +61,7 @@ var questionList = {
              "Africa"]   
          },
      
-      film5: {
+       {
          question: "In what movie, if you watch a certain video, will you die in 7 days?",
          answers: [
              "The Grudge",
@@ -70,7 +71,7 @@ var questionList = {
              "The Ring"]
          },   
          
-      film6: {    
+       {    
          question: "Bruce Willis played a convict turned time traveler in what 1995 movie?",
          answers: [
               "Time Bandits",
@@ -80,7 +81,7 @@ var questionList = {
              "12 Monkeys"]
          },
          
-      film7: {
+       {
          question: "What popular movie musical, directed by John Hutson, was released in 1982?",
          answers: [
              "Victor/Victoria",
@@ -90,7 +91,7 @@ var questionList = {
              "Annie"]
          },   
      
-      film8: {
+       {
          question: "In which year were the Academy Awards, or 'Oscars', first presented?",
          answers: [
              "1929",
@@ -100,7 +101,7 @@ var questionList = {
              "1929"]
          },   
            
-      film9: {
+       {
          question: "What is the name of the kingdom where the 2013 animated movie Frozen is set?",
          answers: [
              "Avalon",
@@ -110,7 +111,7 @@ var questionList = {
              "Arendelle"]
          },   
           
-      film10: {
+       {
          question: "Which Tom Hanks movie won the Academy Award for Best Picture in 1994?",
          answers: [
              "Forrest Gump",
@@ -119,7 +120,7 @@ var questionList = {
              "Big",
              "Forrest Gump"]
     }
-};
+];
 
     $("#start-over").hide();
 
@@ -133,14 +134,14 @@ function run() {
 //  The decrement function.
 function decrement() {
 
-//  Decrease number by one.
-    number--;
+//  Decrease timer by one.
+    timer--;
 
-//  Show the number in the #countdown tag.
-    $("#countdown").html("<h2>" + "Time Remaining: " + number + "</h2>");
+//  Show the time remaining in the #countdown tag.
+    $("#countdown").html("<h2>" + "Time Remaining: " + timer + "</h2>");
 
-    //  Once number hits zero...
-    if (number === 0) {
+    //  Once the timer hits zero...
+    if (timer === 0) {
 
         //  ...run the stop function.
         stop();
@@ -154,7 +155,7 @@ function stop() {
     clearInterval(intervalId);
 
 //  Let the user know time's up
-    if (number === 0) {
+    if (timer === 0) {
     $("#countdown").html("<h2>" + "TIME'S UP!" + "</h2>");
     };
 
@@ -162,8 +163,8 @@ function stop() {
 
     //show next question and possible answers
 
-    //  Set The number counter back to 21.
-    number = 21;
+    //  Set The timer back to 21 seconds
+    timer = 21;
 
     //Add 1 to unanswered questions counter
     unAnswered++;
@@ -176,33 +177,7 @@ function stop() {
 $("#start").on("click", function () {
 
     $("#start").hide();
-
-    $("#question").text(questionList.film1.question);
-    $("#answer1").text(questionList.film1.answers[0]);
-    $("#answer1").show();
-    $("#answer2").text(questionList.film1.answers[1]);
-    $("#answer2").show();
-    $("#answer3").text(questionList.film1.answers[2]);
-    $("#answer3").show();
-    $("#answer4").text(questionList.film1.answers[3]);
-    $("#answer4").show();
-
-    //  Execute the run function.
-    run();
-
-    correct = questionList.film1.answers[4];
-    
-    screenCorrectAnswer = 0;
-
-    if (correct === questionList.film1.answers[0]) {
-        screenCorrectAnswer = 1;
-        } else if (correct === questionList.film1.answers[1]) {
-        screenCorrectAnswer = 2;
-        } else if (correct === questionList.film1.answers[2]) {
-            screenCorrectAnswer = 3;
-        } else {
-            screenCorrectAnswer = 4;
-        }
+    questionAnswer(qcounter);
 });
 
     $(".answer-value").on("click", function () {
@@ -217,4 +192,36 @@ $("#start").on("click", function () {
         }
 
         stop();
+        qcounter++;
+        questionAnswer(qcounter);
     })
+
+    function questionAnswer(qcounter) {
+        
+    $("#question").text(questionList[qcounter].question);
+    $("#answer1").text(questionList[qcounter].answers[0]);
+    $("#answer1").show();
+    $("#answer2").text(questionList[qcounter].answers[1]);
+    $("#answer2").show();
+    $("#answer3").text(questionList[qcounter].answers[2]);
+    $("#answer3").show();
+    $("#answer4").text(questionList[qcounter].answers[3]);
+    $("#answer4").show();
+
+    //  Execute the run function.
+    run();
+    
+    correct = questionList[qcounter].answers[4];
+    
+    screenCorrectAnswer = 0;
+
+    if (correct === questionList[qcounter].answers[0]) {
+        screenCorrectAnswer = 1;
+        } else if (correct === questionList[qcounter].answers[1]) {
+        screenCorrectAnswer = 2;
+        } else if (correct === questionList[qcounter].answers[2]) {
+            screenCorrectAnswer = 3;
+        } else {
+            screenCorrectAnswer = 4;
+        }
+    }
